960ef9a358d8046655531356ae27b925a42fc96e
aftix <aftix@aftix.xyz>
2020-12-08 12:17:03 -0600

    Made roots generic

--- src/roots/mod.rs
+++ src/roots/mod.rs
@@ -4,8 +4,8 @@
  * See repository LICENSE for information.
  */
 
-use nalgebra::DVector;
-use num_traits::{Float, NumCast, Zero};
+use nalgebra::{DVector};
+use alga::general::{ComplexField,RealField};
 
 /// Use the bisection method to solve for a zero of an equation.
 ///
@@ -38,7 +38,7 @@ use num_traits::{Float, NumCast, Zero};
 ///   let solution = bisection((-1.0, 1.0), cubic, 0.001, 1000).unwrap();
 /// }
 /// ```
-pub fn bisection<N: Float+NumCast+Zero>((mut left, mut right): (N, N), f: fn(N) -> N, tol: N, n_max: usize) -> Result<N, String>
+pub fn bisection<N: RealField+From<f64>>((mut left, mut right): (N, N), f: fn(N) -> N, tol: N, n_max: usize) -> Result<N, String>
 {
   if left >= right {
     return Err("requirement: right > left".to_owned());
@@ -47,15 +47,11 @@ pub fn bisection<N: Float+NumCast+Zero>((mut left, mut right): (N, N), f: fn(N) 
   let mut n = 1;
 
   let mut f_a = f(left);
-  if f_a * f(right) > Zero::zero() {
+  if (f_a * f(right)).is_sign_positive() {
     return Err("requirement: Signs must be different".to_owned());
   }
 
-  if f_a * f(right) > Zero::zero() {
-    return Err("sgn(f(a)) != sgn(f(b))".to_owned());
-  }
-
-  let mut half_interval = (left - right) * N::from(0.5f64).unwrap();
+  let mut half_interval = (left - right) * N::from(0.5);
   let mut middle = left + half_interval;
 
   if middle.abs() <= tol {
@@ -64,18 +60,18 @@ pub fn bisection<N: Float+NumCast+Zero>((mut left, mut right): (N, N), f: fn(N) 
 
   while n <= n_max {
     let f_p = f(middle);
-    if f_p * f_a > Zero::zero() {
+    if (f_p * f_a).is_sign_positive() {
       left = middle;
       f_a = f_p;
     } else {
       right = middle;
     }
 
-    half_interval = (right - left) * N::from(0.5f64).unwrap();
+    half_interval = (right - left) * N::from(0.5);
 
     let middle_new = left + half_interval;
 
-    if (middle - middle_new).abs() / middle.abs() < tol {
+    if (middle - middle_new).abs() / middle.abs() < tol || middle_new.abs() < tol {
       return Ok(middle_new);
     }
 
@@ -123,18 +119,18 @@ pub fn bisection<N: Float+NumCast+Zero>((mut left, mut right): (N, N), f: fn(N) 
 ///   let solution = newton(&[0.1], cubic, cubic_deriv, 0.001, 1000).unwrap();
 /// }
 /// ```
-pub fn newton(
-  initial: &[f64],
-  f: fn(&[f64]) -> DVector<f64>,
-  f_deriv: fn(&[f64]) -> DVector<f64>,
-  tol: f64,
+pub fn newton<N: RealField, M: ComplexField+Into<N>>(
+  initial: &[M],
+  f: fn(&[M]) -> DVector<M>,
+  f_deriv: fn(&[M]) -> DVector<M>,
+  tol: N,
   n_max: usize
-) -> Result<DVector<f64>, String> {
+) -> Result<DVector<M>, String> {
   let mut guess = DVector::from_column_slice(initial);
-  let mut norm = guess.norm();
+  let mut norm: N = guess.dot(&guess).sqrt().into();
   let mut n = 0;
 
-  if approx_eq!(f64, 0.0, norm, epsilon = tol) {
+  if norm <= tol {
     return Ok(guess);
   }
 
@@ -143,11 +139,11 @@ pub fn newton(
     let f_deriv_val = f_deriv(guess.column(0).as_slice());
     let adjustment = DVector::from_iterator(
       guess.column(0).len(),
-      f_val.column(0).iter().zip(f_deriv_val.column(0).iter()).map(|(f, f_d)| f / f_d)
+      f_val.column(0).iter().zip(f_deriv_val.column(0).iter()).map(|(f, f_d)| *f / *f_d)
     );
     let new_guess = &guess - adjustment;
-    let new_norm = new_guess.norm();
-    if ((norm - new_norm) / norm).abs() < tol || approx_eq!(f64, 0.0, new_norm, epsilon = tol){
+    let new_norm = new_guess.dot(&new_guess).sqrt().into();
+    if ((norm - new_norm) / norm).abs() <= tol || new_norm <= tol {
       return Ok(new_guess);
     }
 
@@ -190,12 +186,12 @@ pub fn newton(
 ///   let solution = secant((&[0.1], &[-0.1]), cubic, 0.001, 1000).unwrap();
 /// }
 /// ```
-pub fn secant(
-  initial: (&[f64], &[f64]),
-  f: fn(&[f64]) -> DVector<f64>,
-  tol: f64,
+pub fn secant<N: RealField+From<f64>, M: ComplexField+Into<N>>(
+  initial: (&[M], &[M]),
+  f: fn(&[M]) -> DVector<M>,
+  tol: N,
   n_max: usize
-) -> Result<DVector<f64>, String> {
+) -> Result<DVector<M>, String> {
   let mut n = 0;
 
   let mut left = DVector::from_column_slice(initial.0);
@@ -204,8 +200,8 @@ pub fn secant(
   let mut left_val = f(initial.0);
   let mut right_val = f(initial.1);
 
-  let mut norm = right.norm();
-  if approx_eq!(f64, 0.0, norm, epsilon = tol) {
+  let mut norm = right.dot(&right).sqrt().into();
+  if norm <= tol {
     return Ok(right);
   }
 
@@ -213,12 +209,12 @@ pub fn secant(
     let adjustment = DVector::from_iterator(
       right_val.iter().len(),
       right_val.iter().enumerate().map(|(i, q)| {
-        q * (*right.get(i).unwrap() - *left.get(i).unwrap()) / (q - *left_val.get(i).unwrap())
+        *q * (*right.get(i).unwrap() - *left.get(i).unwrap()) / (*q - *left_val.get(i).unwrap())
       })
     );
     let new_guess = &right - adjustment;
-    let new_norm = new_guess.norm();
-    if ((norm - new_norm) / norm).abs() < tol || approx_eq!(f64, 0.0, new_norm, epsilon = tol) {
+    let new_norm = new_guess.dot(&new_guess).sqrt().into();
+    if ((norm - new_norm) / norm).abs() <= tol || new_norm <= tol {
       return Ok(new_guess);
     }
 
