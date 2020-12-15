47ccd2901808b564a17fa1a816c0c60946eb9fff
aftix <aftix@aftix.xyz>
2020-12-15 13:20:54 -0600

    Clamped cubic spline

--- src/interp/spline.rs
+++ src/interp/spline.rs
@@ -84,7 +84,7 @@ pub fn spline_free<N: ComplexField>(
         hs.push(xs[i + 1] - xs[i]);
     }
 
-    if hs.iter().find(|h| !h.is_sign_positive()).is_some() {
+    if hs.iter().any(|h| !h.is_sign_positive()) {
         return Err("spline_free: xs must be sorted".to_owned());
     }
 
@@ -143,10 +143,131 @@ pub fn spline_free<N: ComplexField>(
         poly += b_coefficient[i];
         poly *= term;
         poly += ys[i];
-        println!(
-            "{:?} {} {} {} {}",
-            poly, ys[i], b_coefficient[i], c_coefficient[i], d_coefficient[i]
+        polynomials.push(poly);
+        ranges.push((xs[i], xs[i + 1]));
+    }
+
+    Ok(CubicSpline {
+        cubics: polynomials,
+        ranges,
+    })
+}
+
+/// Create a clamped cubic spline interpolating the given points.
+///
+/// Given a set of ordered points, produce a piecewise function of
+/// cubic polynomials that interpolate the points given the first derivative
+/// of the piecewise function at the end points is the same as the given values
+/// and the piecewise function is smooth.
+///
+/// # Params
+/// `xs` x points. Must be real because cubic splines keep track of ranges within
+/// which it interpolates. Must be sorted.
+///
+/// `ys` y points. Can be complex. ys[i] must match with xs[i].
+///
+/// `(f_0, f_n)` The derivative values at the end points.
+///
+/// `tol` the tolerance of the polynomials
+///
+/// # Examples
+/// ```
+/// use bacon_sci::interp::spline_clamped;
+/// fn example() {
+///     let xs: Vec<_> = (0..=10).map(|x| x as f64).collect();
+///     let ys: Vec<_> = xs.iter().map(|x| x.exp()).collect();
+///
+///     let spline = spline_clamped(&xs, &ys, (1.0, (10.0f64).exp()), 1e-8).unwrap();
+///     for i in 0..1000 {
+///         let i = i as f64 * 0.001;
+///         assert!((spline.evaluate(i).unwrap() - i.exp()).abs() / i.exp() < 0.25);
+///     }
+/// }
+/// ```
+pub fn spline_clamped<N: ComplexField>(
+    xs: &[N::RealField],
+    ys: &[N],
+    (f_0, f_n): (N, N),
+    tol: N::RealField,
+) -> Result<CubicSpline<N>, String> {
+    if xs.len() != ys.len() {
+        return Err("spline_clamped: xs and ys must be same length".to_owned());
+    }
+    if xs.len() < 2 {
+        return Err("spline_clamped: need at least two points".to_owned());
+    }
+
+    let mut hs = Vec::with_capacity(xs.len() - 1);
+    for i in 0..xs.len() - 1 {
+        hs.push(xs[i + 1] - xs[i]);
+    }
+
+    if hs.iter().any(|h| !h.is_sign_positive()) {
+        return Err("spline_clamped: xs must be sorted".to_owned());
+    }
+
+    let mut alphas = vec![N::zero(); xs.len()];
+    alphas[0] = N::from_f64(3.0).unwrap() * ((ys[1] - ys[0]) / N::from_real(hs[0]) - f_0);
+    alphas[xs.len() - 1] = N::from_f64(3.0).unwrap()
+        * (f_n - (ys[xs.len() - 1] - ys[xs.len() - 2]) / N::from_real(hs[xs.len() - 2]));
+
+    for i in 1..xs.len() - 1 {
+        alphas[i] = N::from_f64(3.0).unwrap()
+            * ((ys[i + 1] - ys[i]) / N::from_real(hs[i])
+                - (ys[i] - ys[i - 1]) / N::from_real(hs[i - 1]));
+    }
+
+    let mut l = Vec::with_capacity(xs.len() - 1);
+    let mut mu = Vec::with_capacity(xs.len() - 1);
+    let mut z = Vec::with_capacity(xs.len() - 1);
+
+    l.push(N::from_f64(2.0).unwrap() * N::from_real(hs[0]));
+    mu.push(N::from_f64(0.5).unwrap());
+    z.push(alphas[0] / l[0]);
+
+    for i in 1..xs.len() - 1 {
+        l.push(
+            N::from_f64(2.0).unwrap() * N::from_real(xs[i + 1] - xs[i - 1])
+                - N::from_real(hs[i - 1]) * mu[i - 1],
         );
+        mu.push(N::from_real(hs[i]) / l[i]);
+        z.push((alphas[i] - N::from_real(hs[i - 1]) * z[i - 1]) / l[i]);
+    }
+
+    l.push(N::from_real(hs[xs.len() - 2]) * (N::from_f64(2.0).unwrap() - mu[xs.len() - 2]));
+    z.push(
+        (alphas[xs.len() - 1] - N::from_real(hs[xs.len() - 2]) * z[xs.len() - 2]) / l[xs.len() - 1],
+    );
+
+    let mut b_coefficient = vec![N::zero(); xs.len()];
+    let mut c_coefficient = vec![N::zero(); xs.len()];
+    let mut d_coefficient = vec![N::zero(); xs.len()];
+
+    c_coefficient[xs.len() - 1] = z[xs.len() - 1];
+
+    for i in (0..xs.len() - 1).rev() {
+        c_coefficient[i] = z[i] - mu[i] * c_coefficient[i + 1];
+        b_coefficient[i] = (ys[i + 1] - ys[i]) / N::from_real(hs[i])
+            - N::from_real(hs[i])
+                * N::from_f64(1.0 / 3.0).unwrap()
+                * (c_coefficient[i + 1] + N::from_f64(2.0).unwrap() * c_coefficient[i]);
+        d_coefficient[i] = (c_coefficient[i + 1] - c_coefficient[i])
+            / (N::from_f64(3.0).unwrap() * N::from_real(hs[i]));
+    }
+
+    let mut polynomials = Vec::with_capacity(xs.len() - 1);
+    let mut ranges = Vec::with_capacity(xs.len() - 1);
+
+    for i in 0..xs.len() - 1 {
+        // Horner's method to build polynomial
+        let term = polynomial![N::one(), N::from_real(-xs[i])];
+        let mut poly = &term * d_coefficient[i];
+        poly.set_tolerance(tol)?;
+        poly += c_coefficient[i];
+        poly *= &term;
+        poly += b_coefficient[i];
+        poly *= term;
+        poly += ys[i];
         polynomials.push(poly);
         ranges.push((xs[i], xs[i + 1]));
     }
--- src/tests/interp/spline.rs
+++ src/tests/interp/spline.rs
@@ -1,4 +1,4 @@
-use crate::interp::spline_free;
+use crate::interp::{spline_clamped, spline_free};
 
 #[test]
 fn free_spline() {
@@ -7,8 +7,21 @@ fn free_spline() {
 
     let spline = spline_free(&xs, &ys, 1e-10).unwrap();
 
-    for i in 0..500 {
+    for i in 0..1000 {
         let i = i as f64 * 0.01;
-        assert!((spline.evaluate(i).unwrap() - i.exp()).abs() / i.exp() < 0.25);
+        assert!((spline.evaluate(i).unwrap() - i.exp()).abs() / i.exp() < 0.1);
+    }
+}
+
+#[test]
+fn clamped_spline() {
+    let xs: Vec<_> = (0..=10).map(|x| x as f64).collect();
+    let ys: Vec<_> = xs.iter().map(|x| x.exp()).collect();
+
+    let spline = spline_clamped(&xs, &ys, (1.0, (10.0f64).exp()), 1e-10).unwrap();
+
+    for i in 0..1000 {
+        let i = i as f64 * 0.01;
+        assert!((spline.evaluate(i).unwrap() - i.exp()).abs() / i.exp() < 0.05);
     }
 }
