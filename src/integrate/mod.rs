e094855b6ae815675182ee98ef34263eade3d2b4
aftix <aftix@aftix.xyz>
2020-12-21 16:46:14 -0600

    Adaptive simpson's rule

--- src/integrate/mod.rs
+++ src/integrate/mod.rs
@@ -4,8 +4,112 @@
  * See repository LICENSE for information.
  */
 
-use alga::general::ComplexField;
+use alga::general::{ComplexField, RealField};
 use num_traits::FromPrimitive;
+
+pub fn integrate<N: ComplexField>(
+    left: N::RealField,
+    right: N::RealField,
+    f: fn(N::RealField) -> N,
+    tol: N::RealField,
+    n_max: usize,
+) -> Result<N, String> {
+    if left >= right {
+        return Err("integrate: left must be less than right".to_owned());
+    }
+    if !tol.is_sign_positive() {
+        return Err("integrate: tolerance must be positive".to_owned());
+    }
+    let mut area = N::zero();
+    let mut i = 1;
+    let mut tol_i = vec![N::RealField::from_i32(10).unwrap() * tol];
+    let mut left_i = vec![left];
+    let mut step_i = vec![(right - left) * N::RealField::from_f64(0.5).unwrap()];
+    let mut f_ai = vec![f(left)];
+    let mut f_ci = vec![f(left + step_i[0])];
+    let mut f_bi = vec![f(right)];
+    let mut sum_i = vec![
+        N::from_real(step_i[0])
+            * (f_ai[0] + N::from_i32(4).unwrap() * f_ci[0] + f_bi[0])
+            * N::from_f64(1.0 / 3.0).unwrap(),
+    ];
+    let mut l_i = vec![1];
+
+    while i > 0 {
+        let f_d = f(left_i[i - 1] + N::RealField::from_f64(0.5).unwrap() * step_i[i - 1]);
+        let f_e = f(left_i[i - 1] + N::RealField::from_f64(1.5).unwrap() * step_i[i - 1]);
+        let s1 = N::from_real(step_i[i - 1])
+            * (f_ai[i - 1] + N::from_i32(4).unwrap() * f_d + f_ci[i - 1])
+            * N::from_f64(1.0 / 6.0).unwrap();
+        let s2 = N::from_real(step_i[i - 1])
+            * (f_ci[i - 1] + N::from_i32(4).unwrap() * f_e + f_bi[i - 1])
+            * N::from_f64(1.0 / 6.0).unwrap();
+        let v_1 = left_i[i - 1];
+        let v_2 = f_ai[i - 1];
+        let v_3 = f_ci[i - 1];
+        let v_4 = f_bi[i - 1];
+        let v_5 = step_i[i - 1];
+        let v_6 = tol_i[i - 1];
+        let v_7 = sum_i[i - 1];
+        let v_8 = l_i[i - 1];
+
+        i -= 1;
+
+        if (s1 + s2 - v_7).abs() < v_6 {
+            area += s1 + s2;
+        } else {
+            println!("{} {}", (s1 + s2 - v_7).abs(), v_6);
+            if v_8 >= n_max {
+                println!("{}", v_8);
+                return Err("integrate: maximum iterations exceeded".to_owned());
+            }
+
+            i += 1;
+            if i > left_i.len() {
+                left_i.push(v_1 + v_5);
+                f_ai.push(v_3);
+                f_ci.push(f_e);
+                f_bi.push(v_4);
+                step_i.push(N::RealField::from_f64(0.5).unwrap() * v_5);
+                tol_i.push(N::RealField::from_f64(0.5).unwrap() * v_6);
+                sum_i.push(s2);
+                l_i.push(v_8 + 1);
+            } else {
+                left_i[i - 1] = v_1 + v_5;
+                f_ai[i - 1] = v_3;
+                f_ci[i - 1] = f_e;
+                f_bi[i - 1] = v_4;
+                step_i[i - 1] = N::RealField::from_f64(0.5).unwrap() * v_5;
+                tol_i[i - 1] = N::RealField::from_f64(0.5).unwrap() * v_6;
+                sum_i[i - 1] = s2;
+                l_i[i - 1] = v_8 + 1;
+            }
+
+            i += 1;
+            if i > left_i.len() {
+                left_i.push(v_1);
+                f_ai.push(v_2);
+                f_ci.push(f_d);
+                f_bi.push(v_3);
+                step_i.push(step_i[i - 2]);
+                tol_i.push(tol_i[i - 2]);
+                sum_i.push(s1);
+                l_i.push(l_i[i - 2]);
+            } else {
+                left_i[i - 1] = v_1;
+                f_ai[i - 1] = v_2;
+                f_ci[i - 1] = f_d;
+                f_bi[i - 1] = v_3;
+                step_i[i - 1] = step_i[i - 2];
+                tol_i[i - 1] = tol_i[i - 2];
+                sum_i[i - 1] = sum_i[i - 2];
+                l_i[i - 1] = l_i[i - 2];
+            }
+        }
+    }
+
+    Ok(area)
+}
 
 /// Numerically integrate a function over an interval.
 ///
@@ -18,7 +122,7 @@ pub fn integrate_fixed<N: ComplexField>(
     n: usize,
 ) -> Result<N, String> {
     if left >= right {
-        return Err("integrate: left must be less than right".to_owned());
+        return Err("integrate_fixed: left must be less than right".to_owned());
     }
 
     let mut h = right - left;
--- src/tests/integrate/mod.rs
+++ src/tests/integrate/mod.rs
@@ -1,4 +1,4 @@
-use crate::integrate::integrate_fixed;
+use crate::integrate::{integrate, integrate_fixed};
 use std::f64;
 
 fn exp(x: f64) -> f64 {
@@ -11,6 +11,21 @@ fn sin(x: f64) -> f64 {
 
 fn sinsin(x: f64) -> f64 {
     x.sin().sin()
+}
+
+#[test]
+fn test_integrate() {
+    let area = integrate(0.0, f64::consts::PI, sin, 0.00001, 50).unwrap();
+    assert!(approx_eq!(f64, area, 2.0, epsilon = 0.0001));
+    let area = integrate(0.0, 10.0, exp, 0.00001, 50).unwrap();
+    assert!(approx_eq!(
+        f64,
+        area,
+        (10.0 as f64).exp() - 1.0,
+        epsilon = 0.0001
+    ));
+    let area = integrate(0.0, f64::consts::PI, sinsin, 0.00001, 50).unwrap();
+    assert!(approx_eq!(f64, area, 1.78649, epsilon = 0.0001));
 }
 
 #[test]
