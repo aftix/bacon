0de3a7a3187bcd0f9b51b0357690a89730196719
aftix <aftix@aftix.xyz>
2023-09-02 17:30:18 -0500

    Updated nalgebra version

--- src/tests/ivp/rk.rs
+++ src/tests/ivp/rk.rs
@@ -7,10 +7,6 @@
 use crate::ivp::*;
 use nalgebra::SVector;
 
-fn exp_deriv(_: f64, y: &[f64], _: &mut ()) -> Result<SVector<f64, 1>, String> {
-    Ok(SVector::<f64, 1>::from_column_slice(y))
-}
-
 fn quadratic_deriv(t: f64, _y: &[f64], _: &mut ()) -> Result<SVector<f64, 1>, String> {
     Ok(SVector::<f64, 1>::from_column_slice(&[-2.0 * t]))
 }
@@ -77,38 +73,6 @@ fn rkf_test_sine() -> Result<(), String> {
                     step.1.column(0)[0],
                     step.0.sin(),
                     epsilon = 0.01
-                ));
-            }
-        }
-        Err(string) => panic!("Result not Ok: {}", string),
-    }
-
-    Ok(())
-}
-
-#[test]
-fn bs_test_exp() -> Result<(), String> {
-    let t_initial = 0.0;
-    let t_final = 5.0;
-
-    let solver = RK23::new()
-        .with_dt_min(0.001)?
-        .with_dt_max(0.01)?
-        .with_start(t_initial)?
-        .with_end(t_final)?
-        .with_tolerance(0.01)?
-        .with_initial_conditions(&[1.0])?;
-
-    let path = solver.solve_ivp(&exp_deriv, &mut ());
-
-    match path {
-        Ok(path) => {
-            for step in &path {
-                assert!(approx_eq!(
-                    f64,
-                    step.1.column(0)[0],
-                    step.0.exp(),
-                    epsilon = 0.001
                 ));
             }
         }
